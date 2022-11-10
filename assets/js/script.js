c1eb0ec508d601c387c2fcd4464b6b18fb85bb69
DSnoad <danialsnoad@hotmail.com>
2022-11-10 20:00:13 +0000

    Add selectors to JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const quizData = [
+const quizQuestions = [
     {
       question: "What is Deadpools real name?",
       a: "Richard Webber",
@@ -41,4 +41,13 @@ const quizData = [
     },
     
 ];
+
+const quiz = document.getElementById("quiz");
+const answerElements = document.querySelectorAll(".answer");
+const questionElement = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitButton = document.getElementById("submit");
   
