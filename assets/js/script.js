016f5163a7e739127e7f1760aae76e3ac1d83119
DSnoad <danialsnoad@hotmail.com>
2022-11-10 19:50:43 +0000

    Add questions to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,44 @@
-
+const quizData = [
+    {
+      question: "What is Deadpools real name?",
+      a: "Richard Webber",
+      b: "Wade Wilson",
+      c: "Slade Wilson",
+      d: "Hal Jordan",
+      correct: "b"
+    },
+    {
+        question: "When was the first Deadpool comic released",
+        a: "2000",
+        b: "2016",
+        c: "2018",
+        d: "1991",
+        correct: "d"
+    },
+    {
+        question: "When was the first Deadpool Movie released",
+        a: "2000",
+        b: "2016",
+        c: "2018",
+        d: "1991",
+        correct: "b"
+    },
+    {
+        question: "Who is Deadpools main Romance/girlfriend in the Movies",
+        a: "Rogue",
+        b: "Storm",
+        c: "Vanessa",
+        d: "Logan",
+        correct: "c"
+    },
+    {
+        question: "Deadpool has bovinophobia, whis is the fear of what..?",
+        a: "Cows",
+        b: "Apples",
+        c: "Driving",
+        d: "Children",
+        correct: "a"
+    },
+    
+];
   
